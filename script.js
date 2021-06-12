89c06b904a01f14a71664cc1adcbc69a388c1959
GerardoDev25 <GerardoDev25@gmail.com>
2021-06-11 22:25:50 -0400

    click in the icon

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hola mundo");
+const iconMenu = document.getElementById("icon-menu");
+const iconHamburger = document.getElementById("iconHamburger");
+const iconClose = document.getElementById("iconClose");
+
+// ? when tha page load
+window.addEventListener("DOMContentLoaded", () => {
+   iconClose.style.display = "none";
+});
+
+// ? when chiking in the icon
+iconMenu.addEventListener("click", (e) => {
+   e.stopPropagation();
+
+   if (iconClose.style.display == "none") {
+      iconClose.style.display = "block";
+      iconHamburger.style.display = "none";
+   } else {
+      iconClose.style.display = "none";
+      iconHamburger.style.display = "block";
+   }
+});
